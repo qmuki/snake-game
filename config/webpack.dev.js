14a1db2cd589ecd7854a949a0e5d4ec5b638db7b
0lun <bahahchuk@yandex.ru>
2018-12-01 13:11:22 +0300

    Add webpack and dev server

---
+++ config/webpack.dev.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	entry: path.resolve(__dirname, '../src/main.ts'),
+	module: {
+		rules: [{
+			use: 'ts-loader',
+			exclude: /node_modules/
+		}]
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/dist'
+	},
+	devServer: {
+		contentBase: path.join(__dirname, '../public'),
+		overlay: true
+	  }
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: path.resolve(__dirname, '../src/main.ts'),
+	module: {
+		rules: [{
+			use: 'ts-loader',
+			exclude: /node_modules/
+		}]
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../dist')
+	}
+};
